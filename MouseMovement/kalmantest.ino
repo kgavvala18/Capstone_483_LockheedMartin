bef749a354ca84f26bb123c3ce7f8a1237505fc9
Will <39605195+William-Tatum@users.noreply.github.com>
2025-03-03 10:48:42 -0600

    Update kalmantest.ino
    Signed-off-by: Will <39605195+William-Tatum@users.noreply.github.com>

--- MouseMovement/kalmantest.ino
+++ MouseMovement/kalmantest.ino
@@ -2,19 +2,87 @@
 #include <Adafruit_BMP280.h>
 #include <Adafruit_LIS3MDL.h>
 #include <Adafruit_LSM6DS33.h>
-#include <Adafruit_LSM6DS3TRC.h>
 #include <Adafruit_SHT31.h>
 #include <Adafruit_Sensor.h>
 #include <PDM.h>
 #include <bluefruit.h>
 #include <math.h>
 
-// ----- Kalman Filter Class -----
+// ---------------------
+// Global Calibration Variables
+// ---------------------
+bool calibrated = false;
+float calibRot[3][3];      // Calibration matrix computed from accelerometer (to align gravity)
+float mag_calibRot[3][3];  // Calibration matrix computed from magnetometer (to align x-axis)
+
+// ---------------------
+// Utility Functions
+// ---------------------
+float vectorMagnitude(const float v[3]) {
+  return sqrt(v[0]*v[0] + v[1]*v[1] + v[2]*v[2]);
+}
+
+void normalizeVector(const float v[3], float result[3]) {
+  float mag = vectorMagnitude(v);
+  result[0] = v[0] / mag;
+  result[1] = v[1] / mag;
+  result[2] = v[2] / mag;
+}
+
+float dotProduct(const float a[3], const float b[3]) {
+  return a[0]*b[0] + a[1]*b[1] + a[2]*b[2];
+}
+
+void crossProduct(const float a[3], const float b[3], float result[3]) {
+  result[0] = a[1]*b[2] - a[2]*b[1];
+  result[1] = a[2]*b[0] - a[0]*b[2];
+  result[2] = a[0]*b[1] - a[1]*b[0];
+}
+
+float calculateAngle(const float a[3], const float b[3]) {
+  float dot = dotProduct(a, b);
+  float magA = vectorMagnitude(a);
+  float magB = vectorMagnitude(b);
+  float cosTheta = dot / (magA * magB);
+  if (cosTheta > 1) cosTheta = 1;
+  if (cosTheta < -1) cosTheta = -1;
+  return acos(cosTheta);
+}
+
+// Standard axis-angle rotation matrix (Rodrigues’ rotation formula)
+void calculateRotationMatrix(const float axis[3], float angle, float R[3][3]) {
+  float normAxis[3];
+  normalizeVector(axis, normAxis);
+  float kx = normAxis[0], ky = normAxis[1], kz = normAxis[2];
+  float c = cos(angle);
+  float s = sin(angle);
+  float v = 1 - c;
+  R[0][0] = c + kx*kx*v;
+  R[0][1] = kx*ky*v - kz*s;
+  R[0][2] = kx*kz*v + ky*s;
+  R[1][0] = ky*kx*v + kz*s;
+  R[1][1] = c + ky*ky*v;
+  R[1][2] = ky*kz*v - kx*s;
+  R[2][0] = kz*kx*v - ky*s;
+  R[2][1] = kz*ky*v + kx*s;
+  R[2][2] = c + kz*kz*v;
+}
+
+// Multiply a 3x3 matrix M by a 3-vector v, result in res[3]
+void multiplyMatrixVector(const float M[3][3], const float v[3], float res[3]) {
+  for (int i = 0; i < 3; i++) {
+    res[i] = M[i][0]*v[0] + M[i][1]*v[1] + M[i][2]*v[2];
+  }
+}
+
+// ---------------------
+// Kalman Filter Class
+// ---------------------
 class KalmanFilter {
   public:
     float Q;   // Process noise covariance
     float R;   // Measurement noise covariance
-    float x;   // Filtered state estimate
+    float x;   // State estimate
     float P;   // Estimation error covariance
 
     KalmanFilter(float processNoise, float measurementNoise, float initialValue) {
@@ -25,118 +93,167 @@ class KalmanFilter {
     }
 
     float update(float measurement) {
-      // Prediction update
       P = P + Q;
-      // Measurement update
-      float K = P / (P + R); // Kalman gain
+      float K = P / (P + R);
       x = x + K * (measurement - x);
       P = (1 - K) * P;
       return x;
     }
 };
 
-// Create one Kalman filter for each axis (tune Q and R as needed)
 KalmanFilter kf_x(0.01, 0.1, 0.0);
 KalmanFilter kf_y(0.01, 0.1, 0.0);
 KalmanFilter kf_z(0.01, 0.1, 0.0);
 
-// ----- Sensor Objects -----
+// ---------------------
+// Sensor Objects
+// ---------------------
 Adafruit_APDS9960 apds9960;
 Adafruit_BMP280 bmp280;     
 Adafruit_LIS3MDL lis3mdl;   
-Adafruit_LSM6DS3TRC lsm6ds3trc; 
 Adafruit_LSM6DS33 lsm6ds33;
 Adafruit_SHT31 sht30;
 
 BLEDis bledis;
 BLEHidAdafruit blehid;
 
-// ----- Adjustable configurations -----
-#define MOVE_STEP         1000
-#define SCROLL_STEP       1
-#define DAMPING           0.9
-#define SCOLL_THRESHOLD   1.0
+// ---------------------
+// Configurations and Globals
+// ---------------------
+#define MOVE_STEP          1000
+#define SCROLL_STEP        1
+#define DAMPING            0.9
+#define SCOLL_THRESHOLD    1.0
 #define POSITION_THRESHOLD 0.001
 
-// Complementary filter coefficient for orientation fusion
-const float comp_alpha = 0.98;
+const float comp_alpha = 0.98; // Complementary filter coefficient
 
-// Integration variables for translation (in world frame)
 float velocity_x = 0.0, velocity_y = 0.0, velocity_z = 0.0;
 float position_x = 0.0, position_y = 0.0, position_z = 0.0;
 unsigned long last_time = 0;
 
-// Gravity estimation (initially set; will be removed by sensor fusion)
-float gravity_x = 0.0, gravity_y = 0.0, gravity_z = 0.0;
-
-uint8_t proximity;
-uint16_t r, g, b, c;
-float temperature, pressure, altitude;
-float magnetic_x, magnetic_y, magnetic_z;
-float accel_x, accel_y, accel_z;
-float gyro_x, gyro_y, gyro_z;
-float humidity;
-long int accel_array[6];
-long int check_array[6]={0, 0, 0, 0, 0, 0};
-
-bool new_rev = true;
-
-// For microphone
-extern PDMClass PDM;
-short sampleBuffer[256];  
-volatile int samplesRead; 
-
-// Variables for sensor fusion (orientation in radians)
+// Orientation variables (in radians)
 float pitch = 0.0, roll = 0.0, yaw = 0.0;
 
-// ----- Rolling Average Filter Setup -----
-// We'll maintain a rolling buffer of the last three linear acceleration values for each axis.
-#define ROLLING_SIZE 3
-float rolling_lin_ax[ROLLING_SIZE] = {0, 0, 0};
-float rolling_lin_ay[ROLLING_SIZE] = {0, 0, 0};
-float rolling_lin_az[ROLLING_SIZE] = {0, 0, 0};
-int rolling_index = 0;
+// ---------------------
+// Calibration Routine
+// ---------------------
+void calibrateSensors() {
+  const int numSamples = 100;
+  float accSum[3] = {0, 0, 0};
+  float magSum[3] = {0, 0, 0};
+  
+  Serial.println("Gathering calibration samples...");
+  for (int i = 0; i < numSamples; i++) {
+    sensors_event_t accel, gyro, temp;
+    lsm6ds33.getEvent(&accel, &gyro, &temp);
+    accSum[0] += accel.acceleration.x;
+    accSum[1] += accel.acceleration.y;
+    accSum[2] += accel.acceleration.z;
+    
+    lis3mdl.read();
+    magSum[0] += lis3mdl.x;
+    magSum[1] += lis3mdl.y;
+    magSum[2] += lis3mdl.z;
+    
+    delay(20);
+  }
+  
+  float accAvg[3] = { accSum[0]/numSamples, accSum[1]/numSamples, accSum[2]/numSamples };
+  float magAvg[3] = { magSum[0]/numSamples, magSum[1]/numSamples, magSum[2]/numSamples };
+  
+  Serial.println("Calibration Averages:");
+  Serial.print("Acc: ");
+  Serial.print(accAvg[0]); Serial.print(", ");
+  Serial.print(accAvg[1]); Serial.print(", ");
+  Serial.println(accAvg[2]);
+  Serial.print("Mag: ");
+  Serial.print(magAvg[0]); Serial.print(", ");
+  Serial.print(magAvg[1]); Serial.print(", ");
+  Serial.println(magAvg[2]);
+  
+  // Reference vectors
+  float gravityRef[3] = {0, 0, -1};  // Gravity points down in world frame
+  float xRef[3] = {1, 0, 0};          // Desired magnetometer x-axis
+  
+  // Normalize measured vectors
+  float normAcc[3];
+  normalizeVector(accAvg, normAcc);
+  float normMag[3];
+  normalizeVector(magAvg, normMag);
+  
+  // --- First Rotation: Align accelerometer (gravity) ---
+  float angleAcc = calculateAngle(normAcc, gravityRef);
+  float adjustedAngle = 2 * PI - angleAcc;
+  float rotAxis[3];
+  crossProduct(normAcc, gravityRef, rotAxis);
+  calculateRotationMatrix(rotAxis, adjustedAngle, calibRot);
+  
+  // --- Second Rotation: Align magnetometer ---
+  float rotatedMag[3];
+  multiplyMatrixVector(calibRot, normMag, rotatedMag);
+  // Project onto X-Y plane and normalize
+  float projMag[3] = { rotatedMag[0], rotatedMag[1], 0 };
+  float projMagNorm[3];
+  normalizeVector(projMag, projMagNorm);
+  
+  float angleMag = calculateAngle(projMagNorm, xRef);
+  float adjustedAngleMag = 2 * PI - angleMag;
+  float magRotAxis[3];
+  crossProduct(projMagNorm, xRef, magRotAxis);
+  calculateRotationMatrix(magRotAxis, adjustedAngleMag, mag_calibRot);
+  
+  Serial.println("Calibration complete.");
+  Serial.println("Calibration Matrix (Acc):");
+  for (int i = 0; i < 3; i++) {
+    Serial.print(calibRot[i][0]); Serial.print(" ");
+    Serial.print(calibRot[i][1]); Serial.print(" ");
+    Serial.println(calibRot[i][2]);
+  }
+  Serial.println("Mag Calibration Matrix:");
+  for (int i = 0; i < 3; i++) {
+    Serial.print(mag_calibRot[i][0]); Serial.print(" ");
+    Serial.print(mag_calibRot[i][1]); Serial.print(" ");
+    Serial.println(mag_calibRot[i][2]);
+  }
+  
+  calibrated = true;
+}
 
-void setup(void) {
+// ---------------------
+// BLE Advertising Setup
+// ---------------------
+void startAdv(void) { 
+  Serial.println("BLE Advertising Started...");
+  Bluefruit.Advertising.addFlags(BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE);
+  Bluefruit.Advertising.addTxPower();
+  Bluefruit.Advertising.addAppearance(BLE_APPEARANCE_HID_MOUSE);
+  Bluefruit.Advertising.addService(blehid);
+  Bluefruit.Advertising.addName();
+  Bluefruit.Advertising.restartOnDisconnect(true);
+  Bluefruit.Advertising.setInterval(32, 244);
+  Bluefruit.Advertising.setFastTimeout(30);
+  Bluefruit.Advertising.start(0);
+}
+
+// ---------------------
+// Setup
+// ---------------------
+void setup() {
   Serial.begin(115200);
   while (!Serial) delay(10);
-  Serial.println("Feather Sense Sensor Demo");
+  Serial.println("Feather Sense Sensor Demo (Calibrated)");
 
-  // Sensor initialization
+  // Initialize sensors
   apds9960.begin();
   apds9960.enableProximity(true);
   apds9960.enableColor(true);
   bmp280.begin();
   lis3mdl.begin_I2C();
   lsm6ds33.begin_I2C();
-  
-  sensors_event_t accel, gyro, temp;
-  lsm6ds33.getEvent(&accel, &gyro, &temp);
-  accel_array[0] = accel.acceleration.x;
-  accel_array[1] = accel.acceleration.y;
-  accel_array[2] = accel.acceleration.z;
-  accel_array[3] = gyro.gyro.x;
-  accel_array[4] = gyro.gyro.y;
-  accel_array[5] = gyro.gyro.z;
-
-  // Use initial accelerometer reading for gravity offset
-  gravity_x = accel.acceleration.x;
-  gravity_y = accel.acceleration.y;
-  gravity_z = accel.acceleration.z;
-  
-  // Check sensor revision
-  for (int i = 0; i < 5; i++) {
-    if (accel_array[i] != check_array[i]) {
-      new_rev = false;
-      break;
-    }
-  }
-  if (new_rev) {
-    lsm6ds3trc.begin_I2C();
-  }
   sht30.begin();
 
-  // Mouse and BLE setup
+  // Initialize BLE and mouse functionality
   Bluefruit.begin();
   Bluefruit.Periph.setConnInterval(9, 16);
   Bluefruit.setTxPower(4);
@@ -146,50 +263,60 @@ void setup(void) {
   blehid.begin();
   startAdv();
 
+  // Perform calibration (ensure the board is steady during this period)
+  Serial.println("Calibrating sensors... Please keep the board steady.");
+  calibrateSensors();
+  
   last_time = millis();
 }
 
-void loop(void) {
-  // --- Read Sensor Data ---
+// ---------------------
+// Main Loop: Sensor Fusion & Mouse Control
+// ---------------------
+void loop() {
+  if (!calibrated) return;  // Wait for calibration (should be done in setup)
+
+  // Read accelerometer data
   sensors_event_t accel, gyro, temp;
-  if (new_rev) {
-    lsm6ds3trc.getEvent(&accel, &gyro, &temp);
-  } else {
-    lsm6ds33.getEvent(&accel, &gyro, &temp);
-  }
-  // Read magnetometer (for yaw)
+  lsm6ds33.getEvent(&accel, &gyro, &temp);
+  float acc_raw[3] = { accel.acceleration.x, accel.acceleration.y, accel.acceleration.z };
+
+  // Apply calibration to accelerometer
+  float acc_cal[3];
+  multiplyMatrixVector(calibRot, acc_raw, acc_cal);
+
+  // Read magnetometer data
   lis3mdl.read();
-  magnetic_x = lis3mdl.x;
-  magnetic_y = lis3mdl.y;
-  magnetic_z = lis3mdl.z;
-  
+  float mag_raw[3] = { lis3mdl.x, lis3mdl.y, lis3mdl.z };
+  // Apply calibration: first apply calibRot then mag_calibRot
+  float tempMag[3];
+  multiplyMatrixVector(calibRot, mag_raw, tempMag);
+  float mag_cal[3];
+  multiplyMatrixVector(mag_calibRot, tempMag, mag_cal);
+
   // Read BMP altitude for vertical correction
   float bmp_alt = bmp280.readAltitude(1013.25);
 
-  // --- Compute time delta ---
+  // Compute time delta
   unsigned long current_time = millis();
   float dt = (current_time - last_time) / 1000.0;
   last_time = current_time;
 
   // --- Sensor Fusion: Estimate Orientation ---
-  // Compute accelerometer angles (in radians)
-  float accel_pitch = atan2(accel.acceleration.y, sqrt(accel.acceleration.x * accel.acceleration.x + accel.acceleration.z * accel.acceleration.z));
-  float accel_roll  = atan2(-accel.acceleration.x, accel.acceleration.z);
-
-  // Update orientation with complementary filter using gyro (assumed in rad/s)
+  // Compute pitch and roll from calibrated accelerometer data
+  float accel_pitch = atan2(acc_cal[1], sqrt(acc_cal[0]*acc_cal[0] + acc_cal[2]*acc_cal[2]));
+  float accel_roll  = atan2(-acc_cal[0], acc_cal[2]);
+  
+  // Complementary filter with gyroscope data (assumed in rad/s)
   pitch = comp_alpha * (pitch + gyro.gyro.y * dt) + (1.0 - comp_alpha) * accel_pitch;
   roll  = comp_alpha * (roll  + gyro.gyro.x * dt) + (1.0 - comp_alpha) * accel_roll;
-  yaw   = atan2(magnetic_y, magnetic_x);  // Yaw from magnetometer (in radians)
+  // Yaw estimated from calibrated magnetometer data
+  yaw = atan2(mag_cal[1], mag_cal[0]);
 
-  // --- Build Rotation Matrix (from body frame to world frame) ---
-  float cosY = cos(yaw);
-  float sinY = sin(yaw);
-  float cosP = cos(pitch);
-  float sinP = sin(pitch);
-  float cosR = cos(roll);
-  float sinR = sin(roll);
-
-  // Using ZYX Euler angles (yaw, pitch, roll)
+  // --- Build Rotation Matrix from Orientation (ZYX Euler angles) ---
+  float cosY = cos(yaw), sinY = sin(yaw);
+  float cosP = cos(pitch), sinP = sin(pitch);
+  float cosR = cos(roll), sinR = sin(roll);
   float R11 = cosY * cosP;
   float R12 = cosY * sinP * sinR - sinY * cosR;
   float R13 = cosY * sinP * cosR + sinY * sinR;
@@ -200,43 +327,26 @@ void loop(void) {
   float R32 = cosP * sinR;
   float R33 = cosP * cosR;
 
-  // --- Transform Accelerometer Reading into World Frame ---
-  float ax = accel.acceleration.x;
-  float ay = accel.acceleration.y;
-  float az = accel.acceleration.z;
-  float world_ax = R11 * ax + R12 * ay + R13 * az;
-  float world_ay = R21 * ax + R22 * ay + R23 * az;
-  float world_az = R31 * ax + R32 * ay + R33 * az;
+  // Transform calibrated accelerometer reading into world frame
+  float world_ax = R11 * acc_cal[0] + R12 * acc_cal[1] + R13 * acc_cal[2];
+  float world_ay = R21 * acc_cal[0] + R22 * acc_cal[1] + R23 * acc_cal[2];
+  float world_az = R31 * acc_cal[0] + R32 * acc_cal[1] + R33 * acc_cal[2];
 
-  // --- Remove Gravity from World Frame Acceleration ---
-  // Gravity assumed 9.81 m/s^2 down along z-axis.
+  // Remove gravity (assume 9.81 m/s² along world z-axis)
   float lin_ax = world_ax;
   float lin_ay = world_ay;
   float lin_az = world_az - 9.81;
 
-  // --- Rolling Average Filter (window size 3) ---
-  // Store current linear acceleration values in the rolling buffers.
-  rolling_lin_ax[rolling_index] = lin_ax;
-  rolling_lin_ay[rolling_index] = lin_ay;
-  rolling_lin_az[rolling_index] = lin_az;
-  rolling_index = (rolling_index + 1) % ROLLING_SIZE;
-  
-  // Compute average over the last three samples.
-  float avg_lin_ax = (rolling_lin_ax[0] + rolling_lin_ax[1] + rolling_lin_ax[2]) / ROLLING_SIZE;
-  float avg_lin_ay = (rolling_lin_ay[0] + rolling_lin_ay[1] + rolling_lin_ay[2]) / ROLLING_SIZE;
-  float avg_lin_az = (rolling_lin_az[0] + rolling_lin_az[1] + rolling_lin_az[2]) / ROLLING_SIZE;
-
-  // --- Kalman Filter on Averaged Linear Acceleration ---
-  float filt_lin_ax = kf_x.update(avg_lin_ax);
-  float filt_lin_ay = kf_y.update(avg_lin_ay);
-  float filt_lin_az = kf_z.update(avg_lin_az);
+  // --- Kalman Filtering on Linear Acceleration ---
+  float filt_lin_ax = kf_x.update(lin_ax);
+  float filt_lin_ay = kf_y.update(lin_ay);
+  float filt_lin_az = kf_z.update(lin_az);
 
   // --- Integration: Update Velocity and Position ---
   velocity_x += filt_lin_ax * dt;
   velocity_y += filt_lin_ay * dt;
   velocity_z += filt_lin_az * dt;
 
-  // Damping to reduce drift
   velocity_x *= DAMPING;
   velocity_y *= DAMPING;
   velocity_z *= DAMPING;
@@ -245,8 +355,7 @@ void loop(void) {
   position_y += velocity_y * dt;
   position_z += velocity_z * dt;
 
-  // --- Correct Vertical Drift with BMP Altitude ---
-  // Blend integrated vertical position with BMP altitude reading.
+  // Correct vertical drift using BMP altitude (blended with complementary filter)
   position_z = comp_alpha * position_z + (1.0 - comp_alpha) * bmp_alt;
 
   // --- Debug Output ---
@@ -267,8 +376,7 @@ void loop(void) {
   Serial.println(position_z);
 
   // --- Mouse Movement Logic ---
-  int move_y = 0;
-  int move_z = 0;
+  int move_y = 0, move_z = 0;
   if (fabs(position_z) > POSITION_THRESHOLD) {
     move_z = (int)(position_z * MOVE_STEP);
   }
@@ -276,52 +384,14 @@ void loop(void) {
     move_y = (int)(position_y * MOVE_STEP);
   }
   blehid.mouseMove(move_y, move_z);
-
-  // Scroll control based on X-axis translation (if outside deadzone)
+  
+  // Scroll control based on X-axis translation
   if (fabs(position_x) > SCOLL_THRESHOLD) {
     if (position_x > 0)
       blehid.mouseScroll(SCROLL_STEP);
     else
       blehid.mouseScroll(-SCROLL_STEP);
   }
-  
+
   delay(100); // Adjust delay for smooth operation
-}
-
-int32_t getPDMwave(int32_t samples) {
-  short minwave = 30000;
-  short maxwave = -30000;
-
-  while (samples > 0) {
-    if (!samplesRead) {
-      yield();
-      continue;
-    }
-    for (int i = 0; i < samplesRead; i++) {
-      minwave = min(sampleBuffer[i], minwave);
-      maxwave = max(sampleBuffer[i], maxwave);
-      samples--;
-    }
-    samplesRead = 0;
-  }
-  return maxwave - minwave;
-}
-
-void onPDMdata() {
-  int bytesAvailable = PDM.available();
-  PDM.read(sampleBuffer, bytesAvailable);
-  samplesRead = bytesAvailable / 2;
-}
-
-void startAdv(void) { 
-  Serial.println("BLE Advertising Started...");
-  Bluefruit.Advertising.addFlags(BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE);
-  Bluefruit.Advertising.addTxPower();
-  Bluefruit.Advertising.addAppearance(BLE_APPEARANCE_HID_MOUSE);
-  Bluefruit.Advertising.addService(blehid);
-  Bluefruit.Advertising.addName();
-  Bluefruit.Advertising.restartOnDisconnect(true);
-  Bluefruit.Advertising.setInterval(32, 244);
-  Bluefruit.Advertising.setFastTimeout(30);
-  Bluefruit.Advertising.start(0);
 }
